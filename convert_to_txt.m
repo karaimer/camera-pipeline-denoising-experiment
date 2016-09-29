c1eb77ccbd670bc5f505c13c0b9064ba7e157d48
karaimer <karaimer@users.noreply.github.com>
2016-09-28 21:53:18 -0400

    Update convert_to_txt.m

--- convert_to_txt.m
+++ convert_to_txt.m
@@ -6,20 +6,46 @@ clear all
 % this way my pipeline can read it, and run through the pipeline. 
 stage4_image = im2double(imread('.\images\syntetic_image.tif')); %image; %
 
-dlmwrite('.\images\syntetic_image\r.txt',stage4_image(:,:,1),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image\g.txt',stage4_image(:,:,2),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image\b.txt',stage4_image(:,:,3),'delimiter',' ', 'precision','%.6f');
+% save image as binary file 
+fd = fopen('.\images\syntetic_image\r.txt','w');
+    fwrite(fd,stage4_image(:,:,1)','double');
+    fclose(fd);
+    
+fd2 = fopen('.\images\syntetic_image\g.txt','w');
+    fwrite(fd2,stage4_image(:,:,2)','double');
+    fclose(fd2);
+    
+fd3 = fopen('.\images\syntetic_image\b.txt','w');
+    fwrite(fd3,stage4_image(:,:,3)','double');
+    fclose(fd3);
 
 
 stage4_imagen = im2double(imread('.\images\syntetic_image_noisy_result_s5.tif')); %image; %
 
-dlmwrite('.\images\syntetic_image_noisy_result_s5\r.txt',stage4_imagen(:,:,1),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image_noisy_result_s5\g.txt',stage4_imagen(:,:,2),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image_noisy_result_s5\b.txt',stage4_imagen(:,:,3),'delimiter',' ', 'precision','%.6f');
-
+% save image as binary file 
+fd = fopen('.\images\syntetic_image_noisy_result_s5\r.txt','w');
+    fwrite(fd,stage4_imagen(:,:,1)','double');
+    fclose(fd);
+    
+fd2 = fopen('.\images\syntetic_image_noisy_result_s5\g.txt','w');
+    fwrite(fd2,stage4_imagen(:,:,2)','double');
+    fclose(fd2);
+    
+fd3 = fopen('.\images\syntetic_image_noisy_result_s5\b.txt','w');
+    fwrite(fd3,stage4_imagen(:,:,3)','double');
+    fclose(fd3);
 
 stage4_imagem = im2double(imread('.\images\syntetic_image_noisy_result_s5_denoised.tif')); %image; %
 
-dlmwrite('.\images\syntetic_image_noisy_result_s5_denoised\r.txt',stage4_imagem(:,:,1),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image_noisy_result_s5_denoised\g.txt',stage4_imagem(:,:,2),'delimiter',' ', 'precision','%.6f');
-dlmwrite('.\images\syntetic_image_noisy_result_s5_denoised\b.txt',stage4_imagem(:,:,3),'delimiter',' ', 'precision','%.6f');
+% save image as binary file 
+fd = fopen('.\images\syntetic_image_noisy_result_s5_denoised\r.txt','w');
+    fwrite(fd,stage4_imagem(:,:,1)','double');
+    fclose(fd);
+    
+fd2 = fopen('.\images\syntetic_image_noisy_result_s5_denoised\g.txt','w');
+    fwrite(fd2,stage4_imagem(:,:,2)','double');
+    fclose(fd2);
+    
+fd3 = fopen('.\images\syntetic_image_noisy_result_s5_denoised\b.txt','w');
+    fwrite(fd3,stage4_imagem(:,:,3)','double');
+    fclose(fd3);
